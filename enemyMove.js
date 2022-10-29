a53d710e37e059ab6f6d2ce672e5583d5270995b
Seth Bailey <seth.bailey@student.nua.ac.uk>
2022-10-29 12:22:21 +0100

    create enemy and code refactoring closes #4

---
+++ enemyMove.js
@@ -0,0 +1,52 @@
+class EnemyMove{
+    
+    constructor()
+    {
+        this.enemyDirection = "right";
+    }
+    
+    moveRight(){
+        enemy.setVelocityX(300);
+
+        enemy.anims.play('enemyWalkRight', true);
+        enemy.setCircle(30, 2, 30);
+        this.enemyDirection = "right"; 
+
+    }
+
+    moveLeft(){
+        enemy.setVelocityX(-300);
+        enemy.anims.play('enemyWalkLeft', true);
+        enemy.setCircle(30, 75, 30);
+        this.enemyDirection = "left"; 
+    }
+
+    jump(){
+        enemy.setVelocityY(-600);
+    }
+
+    idle(){
+        enemy.setVelocityX(0);
+        if (this.enemyDirection === "right")
+        {
+            this.idleRight();
+        }
+        else if (this.enemyDirection === "left")
+        {
+            this.idleLeft();
+        }
+    }
+
+    idleRight(){
+        enemy.setVelocityX(0);
+        enemy.anims.play('enemyIdleR', true);  
+        enemy.setCircle(30, 2, 30);
+        
+    }
+
+    idleLeft(){
+        enemy.setVelocityX(0); 
+        enemy.anims.play('enemyIdleL', true); 
+        enemy.setCircle(30, 75, 30);
+    }
+}
---
+++ playerMove.js
@@ -0,0 +1,69 @@
+class PlayerMove{
+    
+    constructor()
+    {
+        this.direction = "right";
+    }
+
+    moveRight(){
+        player.setVelocityX(300);
+
+        player.anims.play('right', true);
+        this.direction = "right"; 
+        player.setCircle(16, 10, 12);
+    }
+
+    moveLeft(){
+        player.setVelocityX(-300);
+
+        player.anims.play('left', true);
+        this.direction = "left";
+        player.setCircle(16, 25, 12);
+    }
+
+    jump(){
+        player.setVelocityY(-600);
+    }
+
+    idle(){
+        player.setVelocityX(0);
+        if (this.direction === "right")
+        {
+            this.idleRight();
+        }
+        else if (this.direction === "left")
+        {
+            this.idleLeft();
+        }
+    }
+
+    idleRight(){
+        player.anims.play('idleR', true);  
+    }
+
+    idleLeft(){
+        player.anims.play('idleL', true);  
+    }
+
+    strike(){
+        player.setVelocityX(0);
+
+        if (this.direction === "right")
+        {
+            this.strikeRight();
+        }
+        else if (this.direction === "left")
+        {
+            this.strikeLeft();
+        }
+    }
+
+    strikeRight()
+    {
+        player.anims.play('strikeR', true);
+    }
+
+    strikeLeft(){
+        player.anims.play('strikeL', true);   
+    }
+}
